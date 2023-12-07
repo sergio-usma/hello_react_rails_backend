9656bf3db88b7073fa59c3f07c9ed0fbb78144b7
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-07 17:36:59 -0500

    Add API controller /w complementary files

---
+++ test/controllers/api/api_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class Api::ApiControllerTest < ActionDispatch::IntegrationTest
+  test "should get random_greeting" do
+    get api_api_random_greeting_url
+    assert_response :success
+  end
+end
